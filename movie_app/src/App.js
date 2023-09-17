6bc0b87fffd21577eefbfd6c1eb73e0bda1045ed
github_akeenola <akinola.olabisi@gmail.com>
2023-09-16 17:28:17 -0700

    Showing pix

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,19 +1,50 @@
-import React,{useEffect, useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
-import MovieBox from './MovieBox'
+import MovieBox from './MovieBox';
 
-var API_URL = "https://api.themoviedb.org/3/movie/157336?api_key=a23869194e0d7d25294ce4f332ccb7e4";
+//import {Navbar,Container,Nav,Form, FormControl, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+//var API_URL = "https://api.themoviedb.org/3/movie/157336?api_key=a23869194e0d7d25294ce4f332ccb7e4";
+const API_URL_POPULAR = "https://api.themoviedb.org/3/movie/popular?api_key=a23869194e0d7d25294ce4f332ccb7e4";
+//const API_URL_SEARCH ="https://api.themoviedb.org/3/search/movie?api_key=a23869194e0d7d25294ce4f332ccb7e4";
 function App() {
-  const [movies,setMovies] = useState('');
- // const example = ["one","two"];
-  //setMovies(data.results)
+  const [movies,setMovies] = useState(true);
+  
+    
+  //const example = ["one","two"]
+  async function getMovies() {
+    const res = await fetch(API_URL_POPULAR);
+    const data = await res.json();
+    for(var j in data){
+      console.log("The value> ",data[j]);
+    }
+    console.log(data);
+    return setMovies(data.results);
+  }
   useEffect(()=>{
-    fetch(API_URL).then((res)=>(res.json)).then((data)=>(setMovies(data.results)));
+    getMovies();
+    
+
   },[])
+
   return (
-    <div>
-      {movies?.map((movies,id)=>(<MovieBox key = {id}/>))}
+    <>
+    
+   
+    <div className='container'>
+      <div className='grid'>
+          {Array.isArray(movies)?movies.map((moviesReq,id)=>
+            (<MovieBox key = {id}{...moviesReq}/>)):(<h1>Not Showing movies</h1>)}
+      </div>
+      
     </div>
+
+    
+    
+    </>
+    
   );
 }
 
--- movie_app/src/MovieBox.js
+++ movie_app/src/MovieBox.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Modal,Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 var API_URL_IMAGES = "https://image.tmdb.org/t/p/original"
-const MovieBox= ({movie})=>{
-    
+const MovieBox= ({title,poster_path,vote_average,release_date,overview})=>{
+const [show,setShow] = useState(false) ;
+const handleShow=()=>setShow(true);
+const handleClose=()=>setShow(false);
         return (
-          <div>
-            <h5>{movie.backdrop_path}</h5>
-            <img src={API_URL_IMAGES+movie.backdrop_path} alt="my pix name"/>
+          <div className="card text-center bg-secondary ab-3">
+            <div className="card-body">
+                <h5>{title}</h5>
+                <img className="card-img-top"style={{innerWidth:"4em"}} 
+                          src={API_URL_IMAGES+poster_path} alt="my pix name"/>
+                <div className="card-body">
+                    <p>{overview}</p>
+                    
+                    <button type='button' className='btn btn-primary' onClick={handleShow}>View Movie</button>
+                    <Modal show = {show} onHide={handleClose} style={{opacity:1}}>
+                      <Modal.Header closeButtton>
+                        <Modal.Body>
+                        
+                        <h1>{title}</h1>
+                        <h4>IMdb:{vote_average}</h4>
+                        <h5>Release Date:{release_date}</h5>
+                        <br></br>
+                        <h6>Overview</h6>
+                        <h6>{overview}</h6>
+                        </Modal.Body>
+                        <Modal.Footer>
+                          <Button variant="secondary" onClick={handleClose}></Button>
+                        </Modal.Footer>
+                      </Modal.Header>
+                    </Modal>
+                </div>
+            </div>
+            
+            
           </div>
         );
 }
 export default MovieBox;
+
